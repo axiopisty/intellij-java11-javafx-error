cf1ede18ca5a73f3f6bccdd63d842e14331fc081
Elliot Huntington <elliot.huntington@gmail.com>
2018-11-25 11:36:16 -0700

    Converted artifact to Java module

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module intellij.java11.javafx.error {
+  requires java.base;
+  requires javafx.base;
+  requires javafx.graphics;
+  requires javafx.controls;
+
+  exports com.github.axiopisty.intellij.java11.javafx.error;
+}
